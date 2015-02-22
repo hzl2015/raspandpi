f779bcb2d5fe8690eb6341a71a502417b005a536
Fredrik Strömberg <stromberg@insto.org>
2015-02-22 16:09:36 +0100

    Add config reader class

--- dimmer.py
+++ dimmer.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import time
-time.sleep(30)
+import os
+import ConfigParser
+import datetime
+
+class Settings:
+    def __init__(self, pathname=""):
+        if not pathname:
+            self.pathname = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.ini")
+            
+    def read():
+        config = ConfigParser.ConfigParser()
+        config.read(self.pathname)
+        self.start_time = config.get("Start", "Time")
+        self.start_intensity = config.getfloat("Start", "Intensity")
+        self.wakeup_time = config.get("Wakeup", "Time")
+        self.wakeup_intensity = config.getfloat("Wakeup", "Intensity")
+        self.stop_time = config.get("Stop", "Time")
+
+if __name__ == "__main__":
+    settings = Settings()
+    
